d2a00b817f88e204fa4649fc7dd03da31a72ab43
LilianaGallego <lilianagallegom@gmail.com>
2023-08-12 11:07:08 -0500

    build "Estructura base del proyecto"

---
+++ src/main/java/com/swisslub/appSwisslub/AppSwisslubApplication.java
@@ -0,0 +1,13 @@
+package com.swisslub.appSwisslub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSwisslubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSwisslubApplication.class, args);
+	}
+
+}
